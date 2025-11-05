60789b8e5d71a548297db1e2b2eb854c1aec365c
Kiyonory <ltm5607@gmail.com>
2025-11-05 17:39:06 +0300

    fix2

--- dist/utils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const message = "Hello from utils!";
-//# sourceMappingURL=utils.d.ts.map
--- dist/utils.js
+++
@@ -1,2 +0,0 @@
-export const message = 'Hello from utils!';
-//# sourceMappingURL=utils.js.map
